db62bdd3fea2bf8aa4635b407769b56d14830ee5
Hack Reactor Student <student@hackreactor.com>
2017-09-01 12:32:02 -0500

    passed data to app

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,10 @@
 angular.module('video-player')
 
 .component('app', {
-  // TODO
+  templateUrl: 'src/templates/app.html'
+})
+
+.controller('appCtrl', function ($scope) {
+  this.data = window.exampleVideoData;
 });
+
--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,4 +1,7 @@
 angular.module('video-player')
 .component('videoList', {
-  // TODO
+  bindings: {
+    data: '<'
+  },
+  templateUrl: 'src/templates/videoList.html'
 });
